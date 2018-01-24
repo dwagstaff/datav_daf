a4d5672c8b80de0882adce3aca06c100f209cec6
davew <davew@bsquare.com>
2018-01-24 12:18:50 -0800

    Checkpoint

--- docopy.cmd
+++ docopy.cmd
@@ -1,1 +1,2 @@
-xcopy \users\davew\git\paccar\truck-side-tool\tst-data-model\src\main\java/s DaStepRules\src\main\java
+xcopy /y \users\davew\git\paccar\truck-side-tool\tst-data-model\src\main\java/s DaStepRules\src\main\java
+xcopy /y \users\davew\git\paccar\truck-side-tool\tst-data-model\src\main\resources/s DaStepRules\src\main\resources
